8b24589c695c32916042ee293f005698d9e18ccd
elluis <elluis.codes@gmail.com>
2022-12-10 16:46:54 -0600

    markdown implemented using dummy data -
    now creating and hooking up a markdown directory -

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import PostHeader from "./post-header";
+import styles from "./post-content.module.scss";
+import ReactMarkdown from "react-markdown";
 
 
 const DUMMY_POST = {
@@ -13,15 +15,16 @@ const DUMMY_POST = {
 }
 
 
-export default function () {
+export default function PostContent() {
 
     const imagePath = `/images/posts/${ DUMMY_POST.slug }/${ DUMMY_POST.image }`;
 
 
     return (
-        <article>
+        <article className={styles.content}>
             <PostHeader title={ DUMMY_POST.title } image={ imagePath }/>
-            { DUMMY_POST.content }
+            <ReactMarkdown>{ DUMMY_POST.content }</ReactMarkdown>
+
         </article>
     )
 }
