b25404ceff66e94be8242d27446dcd9b96385a6c
elluis <elluis.codes@gmail.com>
2022-12-10 16:46:54 -0600

    markdown implemented using dummy data -
    now creating and hooking up a markdown directory -

--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './post-header.module.scss';
 
-export default function (props) {
+export default function PostHeader(props) {
 
 
     const {title, image} = props;
