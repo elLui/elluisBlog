2d14401d99f698a0267aafe37739ca430d759a01
elluis <elluis.codes@gmail.com>
2022-12-11 22:30:41 -0600

    saving a demo of a very easy handwriting process -
    TODO ::
    need to render a better quality signature for import -
    need to set a timer for section sequence -
    need to write out component structure -

---
+++ testing_ground/vector_signature_demo/script.js
@@ -0,0 +1,5 @@
+const path1 = document.querySelector ("#text_el path").getTotalLength ();
+const path2 = document.querySelector ("#text_L path").getTotalLength ();
+const path3 = document.querySelector ("#text_uis path").getTotalLength ();
+console.log (path1, path2, path3);
+
